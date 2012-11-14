6dd3175dab182c1cd4580c09c6809d9151c62098
Daniel Gomez Jurado <danielgomjur@gmail.com>
2012-11-14 14:53:23 +0100

    Configuration parser added

---
+++ gocm.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"encoding/json"
+)
+
+var configFlag = flag.String("config", "", "config path")
+type Params struct {
+	ApiKey string
+	Port int
+}
+
+
+func main() {
+
+	flag.Parse()
+
+	if *configFlag == "" {
+		fmt.Println("argument required")
+		flag.Usage()
+		return
+	}
+
+	configFile, err := os.Open(*configFlag)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer configFile.Close()
+
+
+	d := json.NewDecoder(configFile)
+	var config *Params
+
+	if err := d.Decode(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if config.ApiKey == "" || config.Port < 8000 {
+		fmt.Println("ApiKey is empty or the port name is incorrect (Port >= 8000)")
+		return
+	}
+
+}
